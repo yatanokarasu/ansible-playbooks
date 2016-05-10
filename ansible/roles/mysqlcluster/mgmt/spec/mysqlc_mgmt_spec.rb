9abb66e647be6dcb91a1097de5d84014419d588f
yatanokarasu <sync.outerphase@gmail.com>
2016-05-10 22:56:29 +0900

    Add test case to check MySQL Cluster user.
    Signed-off-by: yatanokarasu <sync.outerphase@gmail.com>

--- ansible/roles/mysqlcluster/mgmt/spec/mysqlc_mgmt_spec.rb
+++ ansible/roles/mysqlcluster/mgmt/spec/mysqlc_mgmt_spec.rb
@@ -3,7 +3,9 @@ require 'spec_helper'
 context "Test state of MySQL Cluster management node" do
 
   describe user(property['mysqlc_user']) do
-    it { should exist }
+    it { should exist                             }
+    it { should have_uid property['mysqlc_uid']   }
+    it { should have_login_shell '/sbin/nologin'  }
   end
 
 end
